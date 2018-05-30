986b9b8709315476e5c89832652be761b01fec82
UserName <user@mail.com>
2018-05-31 00:27:46 +0700

    Add project files.

---
+++ MainWindow.xaml.fs
@@ -0,0 +1,21 @@
+﻿
+module MainWindow
+
+open System 
+open System.Windows 
+open System.IO
+open System.Windows.Markup
+
+open System.Reflection
+
+let mySr = new StreamReader(Assembly.Load("MainFsharp").GetManifestResourceStream("MainWindow.xaml"))   // XAML - MUST be Embedded Resource 
+let mutable this : Window = XamlReader.Load(mySr.BaseStream):?> Window  // Load XAML
+
+[<STAThread>] 
+[<EntryPoint>]
+
+do (new Application()).Run(this) |> ignore
+
+
+ 
+
---
+++ UserControl/Utilities.fs
@@ -0,0 +1,22 @@
+﻿module Utilities
+
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Media
+
+
+// to bind to Xaml components in code-behind, see example below
+
+let (?) (c:obj) (s:string) =
+    match c with
+    | :? ResourceDictionary as r ->  r.[s] :?> 'T
+    | :? Control as c -> c.FindName(s) :?> 'T
+    | _ -> failwith "dynamic lookup failed"
+
+
+     
+        
+    
+
+        
